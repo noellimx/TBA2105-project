ca016088122cb5a3aa4c7f330eb17679b62de370
Noel Lim <noellimx@gmail.com>
2022-09-13 23:28:15 +0800

    -

--- index.go
+++ index.go
@@ -11,9 +11,7 @@ import (
 )
 
 func basicFatal() {
-
 	log.Fatalf("Error ")
-
 }
 
 var CONFIG_PATH string = "./config.json"
@@ -56,7 +54,7 @@ func (cT *clientT) getExample() {
 
 const myUsername string = "noellimx"
 
-func (ct *clientT) twitterExampleGetUserMe() {
+func (ct *clientT) twitterExampleGetUserMeV2() {
 	fmt.Println("[cT.twitterExample]")
 
 	username := myUsername
@@ -78,10 +76,9 @@ func (ct *clientT) twitterExampleGetUserMe() {
 	statusCode := resp.StatusCode
 
 	fmt.Printf("Status: %d \n", statusCode)
-
 }
 
-func (ct *clientT) twitterExampleRecentSearch(query string) {
+func (ct *clientT) twitterExampleRecentSearchV2(query string) {
 
 	fmt.Println("[cT.twitterExampleRecentSearch]")
 	url := "https://api.twitter.com/2/tweets/search/recent"
@@ -107,7 +104,34 @@ func (ct *clientT) twitterExampleRecentSearch(query string) {
 	body, _ := ioutil.ReadAll(resp.Body)
 
 	fmt.Printf("%s", body)
+}
 
+func (ct *clientT) twitterExampleFullArchiveSearch(query string) {
+
+	fmt.Println("[cT.twitterExampleFullArchiveSearch]")
+	url := "https://api.twitter.com/2/tweets/search/all"
+
+	req, _ := http.NewRequest("GET", url, nil)
+
+	q := req.URL.Query()
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", ct.globalConfig.Twitter.Bearer))
+
+	q.Add("query", query)
+	req.URL.RawQuery = q.Encode()
+	println(req.URL.RawQuery)
+	resp, err := ct.c.Do(req)
+
+	if err != nil {
+		basicFatal()
+	}
+	defer resp.Body.Close()
+
+	statusCode := resp.StatusCode
+
+	fmt.Printf("Status: %d \n", statusCode)
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	fmt.Printf("%s", body)
 }
 
 func main() {
@@ -121,7 +145,9 @@ func main() {
 	}
 
 	cT.getExample()
-	cT.twitterExampleGetUserMe()
+	cT.twitterExampleGetUserMeV2()
 
-	cT.twitterExampleRecentSearch("hello")
+	cT.twitterExampleRecentSearchV2("hello")
+
+	cT.twitterExampleFullArchiveSearch("hello")
 }
