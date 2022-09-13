faf074e3b25b293cf80ed1f7fd9706cd54a59726
Noel Lim <noellimx@gmail.com>
2022-09-13 21:59:27 +0800

    read twitter credentials from config

--- index.go
+++ index.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/noellimx/TBA2105-project.git/config"
+)
+
 var CONFIG_PATH string = "./config.json"
 
+var globalConfig = config.ReadConfig(CONFIG_PATH)
+
 func main() {
+
+	fmt.Printf("Global Config: %+v", globalConfig)
 
 }
