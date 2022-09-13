aad0856e5bb6778d83e6491b95dad5deb4b4a139
Noel Lim <noellimx@gmail.com>
2022-09-13 22:56:13 +0800

    example twitter v2 get username

--- index.go
+++ index.go
@@ -9,6 +9,12 @@ import (
 	"github.com/noellimx/TBA2105-project.git/config"
 )
 
+func basicFatal() {
+
+	log.Fatalf("Error ")
+
+}
+
 var CONFIG_PATH string = "./config.json"
 
 var globalConfig = config.ReadConfig(CONFIG_PATH)
@@ -17,16 +23,18 @@ var haveClient = false
 
 type clientT struct {
 	c *http.Client
+
+	globalConfig *config.GlobalConfig
 }
 
-func NewClientT() (*clientT, error) {
+func NewClientT(globalConfig *config.GlobalConfig) (*clientT, error) {
 	if haveClient {
 
 		return nil, errors.New("math: square root of negative number")
 	}
 
 	haveClient = true
-	return &clientT{c: &http.Client{}}, nil
+	return &clientT{c: &http.Client{}, globalConfig: globalConfig}, nil
 }
 
 func (cT *clientT) getExample() {
@@ -36,7 +44,7 @@ func (cT *clientT) getExample() {
 	resp, err := cT.c.Get("http://example.com")
 
 	if err != nil {
-		log.Fatalf("Error ")
+		basicFatal()
 	}
 
 	defer resp.Body.Close()
@@ -45,16 +53,43 @@ func (cT *clientT) getExample() {
 
 }
 
+const myUsername string = "noellimx"
+
+func (ct *clientT) twitterExample() {
+	fmt.Println("[cT.twitterExample]")
+
+	username := myUsername
+	req, _ := http.NewRequest("GET", fmt.Sprintf("https://api.twitter.com/2/users/by/username/%s", username), nil)
+
+	q := req.URL.Query()
+	q.Add("api_key", "key_from_environment_or_flag")
+	q.Add("another_thing", "foo & bar")
+
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", ct.globalConfig.Twitter.Bearer))
+	println(req.URL.RawQuery)
+	resp, err := ct.c.Do(req)
+
+	if err != nil {
+		basicFatal()
+	}
+	defer resp.Body.Close()
+
+	statusCode := resp.StatusCode
+
+	fmt.Printf("Status: %d \n", statusCode)
+
+}
+
 func main() {
 
 	fmt.Printf("Global Config: %+v \n", globalConfig)
 
-	cT, err := NewClientT()
+	cT, err := NewClientT(globalConfig)
 
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
 
 	cT.getExample()
-
+	cT.twitterExample()
 }
